36138b48c5c6b78630a90a4892f7a868ed276aa6
marco <marco@webguerilla.net>
2015-03-02 16:22:37 +0100

    added exec check to imagewrapper and added file to manifest

--- src/plugin/imagewrapper.php
+++ src/plugin/imagewrapper.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * @copyright  Copyright (C) 2015 Marco Beierer. All rights reserved.
+ * @license    http://www.gnu.org/licenses/agpl-3.0.html GNU/AGPL
+ */
+
+defined('_JEXEC') or die();
+
 class ImageWrapper {
 
 	private $image;
